01ab86343e4e61a4c09705b20625ef8b4aec92c0
Marcone Duarte <marcone.msd15@gmail.com>
2022-08-03 21:16:17 -0300

    estrutura inicial do projeto

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+// const path = require('path')
+
+// const db= require('./database')
+const routes = require('./routes/routes')
+
+const app = express()
+
+
+// CONEXÃO COM O BANCO DE DADOS
+// db.connect()
+
+
+// HABILITA SERVER PARA RECEBER DADOS VIA POST (FORMULARIO)
+app.use(express.urlencoded({ extended: true }))
+
+// DEFININDO AS ROTAS
+app.use('/api', routes)
+
+// EXECUTANDO O SERVIDOR
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
